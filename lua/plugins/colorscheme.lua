69b2c62245c038747d6dde55c44a052f1ba4add3
WuDiXianXin <wudixianxin@163.com>
2026-06-07 19:21:46 +0800

    删除 catppuccin 主题

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,23 +4,9 @@ return {
         'folke/tokyonight.nvim',
         lazy = false,
         priority = 1000,
-        -- config = function()
-        -- moon, night, storm, day
-        -- vim.cmd([[colorscheme tokyonight-moon]])
-        -- end,
-    },
-    {
-        'catppuccin/nvim',
-        lazy = false,
-        name = 'catppuccin',
-        priority = 1000,
         config = function()
-            require('catppuccin').setup({
-                flavour = 'mocha',
-            })
-
-            -- latte, frappe, macchiato, mocha
-            vim.cmd([[colorscheme catppuccin-mocha]])
+            -- moon, night, storm, day
+            vim.cmd([[colorscheme tokyonight-moon]])
         end,
     },
 }
--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -2,14 +2,14 @@
 return {
     'saghen/blink.indent',
     event = 'BufReadPost',
-    -- config = function ()
-    --     require('blink.indent').setup({
-    --     static = {
-    --         char = '▏',
-    --     },
-    --     scope = {
-    --         char = '▏',
-    --     }
-    -- })
-    -- end
+    config = function()
+        require('blink.indent').setup({
+            static = {
+                char = '▏',
+            },
+            scope = {
+                char = '▏',
+            },
+        })
+    end,
 }
